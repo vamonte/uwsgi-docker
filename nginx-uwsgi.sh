0c55ce2b0d63936da07056c98308c55919391e7b
vamonte <valentin.monte@yahoo.fr>
2015-01-22 23:29:09 +0100

    update settings

--- nginx-uwsgi.sh
+++ nginx-uwsgi.sh
@@ -9,17 +9,15 @@ apt-get install -y python-setuptools
 easy_install pip
 pip install uwsgi
 
-## Install Nginx runit service.
+
 mkdir /etc/service/nginx
 cp /build/runit/nginx /etc/service/nginx/run
 cp /build/config/nginx.conf /etc/nginx/nginx.conf
 touch /etc/service/nginx/down
-mkdir /etc/nginx/sites-available
-mkdir /etc/nginx/sites-enabled
+
 mkdir /etc/service/uwsgi
 cp /build/runit/uwsgi /etc/service/uwsgi/run
-cp /build/config/uwsgi.conf /etc/nginx/sites-available/uwsgi.conf
-rm -f /etc/nginx/sites-enabled/default 
-ln -s /etc/nginx/sites-available/uwsgi.conf /etc/nginx/sites-enabled/uwsgi.conf
-touch /etc/service/uwsgi/down
 
+cp /build/config/uwsgi.conf /etc/nginx/conf.d/uwsgi.conf
+rm -f /etc/nginx/conf.d/default.conf
+touch /etc/service/uwsgi/down
